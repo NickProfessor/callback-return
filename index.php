0279cb26f60357e41fe4ba104f5872683f3223ba
Nickollas <nickollassilva90@gmail.com>
2024-09-15 20:22:57 -0300

    projeto base

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+require_once __DIR__ . "/config/config.php";
+require_once __DIR__ . "/config/db_connect.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CallbackReturn</title>
+</head>
+
+<body>
+    <h2>Hello world</h2>
+</body>
+
+</html>
