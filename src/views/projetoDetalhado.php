2cdc647b3b1058baefd3b82c9828e0cedb36ed3e
Nickollas <nickollassilva90@gmail.com>
2024-09-19 17:34:59 -0300

    feat: visualizar projeto detalhadamente

--- src/views/projetoDetalhado.php
+++ src/views/projetoDetalhado.php
@@ -1,10 +1,13 @@
 <div>
     <h2><?php echo htmlspecialchars($projetoNome); ?></h2>
+    <p>Sala <?php echo htmlspecialchars($projetoSala); ?></p>
     <p><?php echo htmlspecialchars($projetoCursos); ?></p>
     <p><?php echo htmlspecialchars($projetoTemas); ?></p>
     <p><?php echo htmlspecialchars($projetoDescricao); ?></p>
+    <p>Integrantes: <?php echo htmlspecialchars($projetoIntegrantes); ?></p>
     <p><?php echo htmlspecialchars($projetoMediaAvaliacoes); ?></p>
     <p><?php echo htmlspecialchars($projetoAvaliacoes); ?></p>
+
     <?php if ($popularAdultos): ?>
         <p>Popular entre adultos</p>
     <?php endif; ?>
@@ -25,9 +28,21 @@
         <p>Popular entre homens</p>
     <?php endif; ?>
 
+    <div>
+        <h3>Comentários:</h3>
+        <?php if (!empty($projetoComentarios)): ?>
+            <ul>
+                <?php foreach ($projetoComentarios as $comentario): ?>
+                    <li><?php echo htmlspecialchars($comentario); ?></li>
+                <?php endforeach; ?>
+            </ul>
+        <?php else: ?>
+            <p>Sem comentários.</p>
+        <?php endif; ?>
+    </div>
 
     <?php if ($projetoId): ?>
         <a href="./avaliaProjeto.php?id=<?php echo $projetoId ?>">Avaliar projeto</a>
-        <a href="./detalhesProjeto.php?id=<?php echo $projetoId ?>">Visualizar detalhadamente</a>
+        <a href="./sala.php?id=<?php echo $projetoSala ?>">Voltar para a sala <?php echo $projetoSala ?></a>
     <?php endif; ?>
 </div>
