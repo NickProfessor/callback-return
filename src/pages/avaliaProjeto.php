fc443f2a318f0dd2060134799980b970294f5332
Nickollas <nickollassilva90@gmail.com>
2024-09-19 17:34:38 -0300

    refeat: recuperando ID caso o usuário tenha esquecido e bloqueando avaliações multiplas

--- src/pages/avaliaProjeto.php
+++ src/pages/avaliaProjeto.php
@@ -3,32 +3,42 @@ $pageTitle = "Avaliando | CallBackReturn";
 include "../views/header.php";
 
 require_once "../models/Projeto.php";
-// Substitua o ID 2 pelo ID dinâmico do projeto que você está avaliando
-$projetoId = 2;
-$projeto = Projeto::obterProjetoPeloId($projetoId);
-?>
 
-<p><?php echo htmlspecialchars($projeto['nome']) ?></p>
 
-<form action="projetoAvaliado.php" method="POST">
-    <!-- Campo oculto para o ID do projeto -->
-    <input type="hidden" name="id_projeto" value="<?php echo htmlspecialchars($projetoId); ?>">
+if (isset($_GET["id"]) && $_GET["id"] != "") {
+    $projetoId = $_GET["id"];
+    $projeto = Projeto::obterProjetoPeloId($projetoId);
+    ?>
 
-    <label for="nota_projeto">Informe a nota</label>
-    <input type="number" name="nota_projeto" id="nota_projeto" required>
+    <p><?php echo htmlspecialchars($projeto['nome']) ?></p>
 
-    <label for="comentario_projeto">Faça algum comentário (opcional)</label>
-    <textarea name="comentario_projeto" id="comentario_projeto" cols="40" rows="6"></textarea>
+    <form action="projetoAvaliado.php" method="POST">
+        <!-- Campo oculto para o ID do projeto -->
+        <input type="hidden" name="id_projeto" value="<?php echo htmlspecialchars($projetoId); ?>">
 
-    <label for="id_usuario">Informe seu número de usuário:</label>
-    <input type="number" name="id_usuario" id="id_usuario" required>
+        <label for="nota_projeto">Informe a nota</label>
+        <input type="number" name="nota_projeto" id="nota_projeto" required>
 
-    <label for="frase">Informe sua frase de segurança</label>
-    <input type="password" name="frase" id="frase" required>
-    
-    <input type="submit" value="Enviar nota">
-</form>
+        <label for="comentario_projeto">Faça algum comentário (opcional)</label>
+        <textarea name="comentario_projeto" id="comentario_projeto" cols="40" rows="6"></textarea>
 
-<?php
+        <label for="id_usuario">Informe seu número de usuário:</label>
+        <input type="number" name="id_usuario" id="id_usuario" required>
+
+        <label for="frase">Informe sua frase de segurança</label>
+        <input type="password" name="frase" id="frase" required>
+
+        <input type="submit" value="Enviar nota">
+    </form>
+
+
+    <button type="button" onclick="history.back()">Voltar</button>
+    <a href="./esqueceuOID.php">Esqueci meu ID</a>
+
+
+    <?php
+} else {
+    echo "Algo deu errado";
+}
 include "../views/footer.php";
 ?>
--- src/pages/projetoAvaliado.php
+++ src/pages/projetoAvaliado.php
@@ -1,22 +1,42 @@
-<?php 
+<?php
 
 $pageTitle = "Avaliado | CallBackReturn";
 include "../views/header.php";
 
 require_once "../models/Avaliacao.php";
 
-$id_projeto = $_POST['id_projeto'];
-$nota_projeto = $_POST['nota_projeto'];
-$comentario_projeto = $_POST['comentario_projeto'];
-$id_usuario = $_POST['id_usuario'];
-$frase = $_POST['frase'];
+if (isset($_POST['id_projeto'], $_POST['nota_projeto'], $_POST['comentario_projeto'], $_POST['id_usuario'], $_POST['frase'])) {
+    // Dados foram passados corretamente
+    $id_projeto = $_POST['id_projeto'];
+    $nota_projeto = $_POST['nota_projeto'];
+    $comentario_projeto = $_POST['comentario_projeto'];
+    $id_usuario = $_POST['id_usuario'];
+    $frase = $_POST['frase'];
 
-$avaliacao = new Avaliacao(
-    $nota_projeto, 
-    $id_projeto, 
-    $comentario_projeto, 
-    $id_usuario, 
-    $frase
-);
+    $avaliacao = new Avaliacao(
+        $nota_projeto,
+        $id_projeto,
+        $comentario_projeto,
+        $id_usuario,
+        $frase
+    );
 
-$avaliacao->avaliaProjeto();
+    try {
+        $sucesso = $avaliacao->avaliaProjeto();
+
+        if ($sucesso) {
+            echo "<p>Sua avaliação foi registrada com sucesso! Obrigado por contribuir.<p>";
+            echo "<a href='../../index.php'>Voltar para tela principal</a>";
+        } else {
+            echo "<p>Não foi possível registrar sua avaliação. Verifique se você já avaliou este projeto ou se os dados estão corretos.</p>";
+            echo "<a href='../../index.php'>Voltar para tela principal</a>";
+        }
+
+    } catch (Exception $e) {
+        echo "Ocorreu um erro: " . $e->getMessage() . "<br>";
+        echo "<a href='../../index.php'>Voltar para tela principal</a>";
+    }
+} else {
+    echo "Erro: Dados incompletos!";
+    echo "<a href='../../index.php'>Voltar para tela principal</a>";
+}
