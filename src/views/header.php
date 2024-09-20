3071d31e998805aa94310db42dccc7f14583e29e
Nickollas <nickollassilva90@gmail.com>
2024-09-20 19:55:33 -0300

    refeact: alterando os diretórios do css

--- src/views/header.php
+++ src/views/header.php
@@ -4,5 +4,12 @@
 <head>
     <meta charset="UTF-8">
     <title><?php echo htmlspecialchars($pageTitle); ?></title>
-    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../assets/css/style.css">
+    <?php if (isset($page)): ?>
+        <?php if ($page === 'confirmarCadastro'): ?>
+            <link rel="stylesheet" href="../assets/css/pages/confirmarCadastro.css">
+        <?php elseif ($page === 'cadastroUsuario'): ?>
+            <link rel="stylesheet" href="../assets/css/pages/cadastroUsuario.css">
+        <?php endif; ?>
+    <?php endif; ?>
 </head>
