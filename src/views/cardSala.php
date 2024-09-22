a19db3fc9224ae7583318cfeac6b21a55874f832
Nickollas <nickollassilva90@gmail.com>
2024-09-22 16:00:15 -0300

    styles: estilizando a tela principal

--- src/views/cardSala.php
+++ src/views/cardSala.php
@@ -1,9 +1,21 @@
-<div>
-    <div>
-        <h2>Sala <?php echo htmlspecialchars($salaNumero); ?></h2>
-        <p>Projetos: <?php echo htmlspecialchars($listaProjetos); ?></p>
-        <p>Total de Avaliações: <?php echo htmlspecialchars($totalAvaliacoes); ?></p>
-        <p>Média das Notas: <?php echo htmlspecialchars(number_format($mediaNotas, 2)); ?></p>
-        <a href='./src/pages/sala.php?id=<?php echo htmlspecialchars($salaNumero); ?>'>Ir para sala</a>
+<div class="sala-padrao">
+    <h2 class="sala-titulo">Sala <?php echo htmlspecialchars($salaNumero); ?></h2>
+    <div class="sala-projetos">
+        <?php
+        foreach ($listaProjetosArray as $projeto) {
+            echo "<p class='sala-projeto'>" . htmlspecialchars($projeto) . "</p>";
+        }
+        ?>
+    </div>
+    <div class="sala-final">
+        <div class="sala-secao-avalia">
+            <i class="fa-solid fa-star"></i>
+            <p class="sala-media">
+                <?php echo htmlspecialchars(number_format($mediaNotas, 1)); ?>
+            </p>
+            <p class="sala-avaliacoes">(<?php echo htmlspecialchars($totalAvaliacoes); ?> avaliações)</p>
+        </div>
+        <a href='./src/pages/sala.php?id=<?php echo htmlspecialchars($salaNumero); ?>' class="botao-padrao">Ir para
+            sala <i class="fa-solid fa-arrow-right"></i></a>
     </div>
 </div>
