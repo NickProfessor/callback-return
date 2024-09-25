9124db9960aa7f9ebb961958a10878cf239600f0
Nickollas <nickollassilva90@gmail.com>
2024-09-25 18:24:10 -0300

    styles: estilizando página de avaliação pt1

--- src/pages/projetoAvaliado.php
+++ src/pages/projetoAvaliado.php
@@ -13,28 +13,33 @@ if (isset($_POST['id_projeto'], $_POST['nota_projeto'], $_POST['comentario_proje
     $id_usuario = $_POST['id_usuario'];
     $frase = $_POST['frase'];
 
-    $avaliacao = new Avaliacao(
-        $nota_projeto,
-        $id_projeto,
-        $comentario_projeto,
-        $id_usuario,
-        $frase
-    );
+    if ($nota_projeto > 10 || $nota_projeto < 1) {
+        header("Location: avaliaProjeto.php?id=$id_projeto&erro=nota-invalida");
+    } else {
 
-    try {
-        $sucesso = $avaliacao->avaliaProjeto();
+        $avaliacao = new Avaliacao(
+            $nota_projeto,
+            $id_projeto,
+            $comentario_projeto,
+            $id_usuario,
+            $frase
+        );
 
-        if ($sucesso) {
-            echo "<p>Sua avaliação foi registrada com sucesso! Obrigado por contribuir.<p>";
-            echo "<a href='../../index.php'>Voltar para tela principal</a>";
-        } else {
-            echo "<p>Não foi possível registrar sua avaliação. Verifique se você já avaliou este projeto ou se os dados estão corretos.</p>";
+        try {
+            $sucesso = $avaliacao->avaliaProjeto();
+
+            if ($sucesso) {
+                echo "<p>Sua avaliação foi registrada com sucesso! Obrigado por contribuir.<p>";
+                echo "<a href='../../index.php'>Voltar para tela principal</a>";
+            } else {
+                echo "<p>Não foi possível registrar sua avaliação. Verifique se você já avaliou este projeto ou se os dados estão corretos.</p>";
+                echo "<a href='../../index.php'>Voltar para tela principal</a>";
+            }
+
+        } catch (Exception $e) {
+            echo "Ocorreu um erro: " . $e->getMessage() . "<br>";
             echo "<a href='../../index.php'>Voltar para tela principal</a>";
         }
-
-    } catch (Exception $e) {
-        echo "Ocorreu um erro: " . $e->getMessage() . "<br>";
-        echo "<a href='../../index.php'>Voltar para tela principal</a>";
     }
 } else {
     echo "Erro: Dados incompletos!";
