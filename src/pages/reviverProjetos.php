167e996f9e9e70284a9aa0a7d3dcec6813398e3a
Nickollas <nickollassilva90@gmail.com>
2025-04-10 14:12:10 -0300

    styles: botões para o gigatech

--- src/pages/reviverProjetos.php
+++ src/pages/reviverProjetos.php
@@ -1,12 +1,18 @@
 <?php
 
+require_once __DIR__ . "/../../config/config.php";
+require_once '../../config/db_connect.php';
+
+
+global $conn;
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['reviver'])) {
-    $sql = "UPDATE projetos SET ativo = 1 WHERE ativo = 0";
-    $stmt = $pdo->prepare($sql);
-    if ($stmt->execute()) {
+    $sql = "UPDATE projeto SET ativo = 1 WHERE ativo = 0";
+    if ($conn->query($sql) === TRUE) {
         echo "Todos os projetos excluídos foram reativados com sucesso!";
     } else {
-        echo "Erro ao reativar os projetos.";
+        echo "Erro ao reativar os projetos: " . $conn->error;
     }
+    echo "<br>";
+    echo "<a href='../../index.php'>Voltar para página principal</a>";
 }
--- src/views/cardProjeto.php
+++ src/views/cardProjeto.php
@@ -47,8 +47,9 @@
             <div class="projeto-secao-acoes">
                 <a href="./src/pages/avaliaProjeto.php?projeto=<?php echo $projetoId ?>" class="projeto-avalia-btn">Avaliar
                     esse
-                    projeto <i class="fa-solid fa-arrow-right"></i></a>
-                <a href="./src/pages/detalhesProjeto.php?projeto=<?php echo $projetoId ?> " class="botao-padrao">Ver
+                    projeto <i class="fa-solid fa-star black-star"></i></a>
+                <a href="./src/pages/detalhesProjeto.php?projeto=<?php echo $projetoId ?> "
+                    class="botao-padrao ver-detalhes-btn">Ver
                     detalhes <i class="fa-solid fa-arrow-right"></i></a>
             </div>
         </div>
